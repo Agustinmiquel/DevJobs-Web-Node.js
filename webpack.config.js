4edbd633c0766630ddcabb34bae24956f1e7b2b8
Agustinmiquel <agumiquel@.com>
2023-11-08 18:54:02 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path")
 const webpack = require("webpack")
 
 module.exports = {
-    entry: './public/js/app.js',  
+    entry: path.resolve(__dirname,'./public/Js/app.js'),  
     output: {
         filename: 'bundle.js',
         path: path.join(__dirname, "./public/dist")
