f9e18674fe2308d572bcf73bce6d747f92a8d7f3
Agustinmiquel <agumiquel@.com>
2023-11-08 21:31:05 -0300

    eliminar

--- controllers/vacantesController.js
+++ controllers/vacantesController.js
@@ -111,9 +111,9 @@ exports.validarVacante = (req, res, next) => {
 
 exports.eliminarVacante = async (req, res) => {
 
-    const { id } = req.params; 
+    const { _id } = req.params; 
 
-    const vacante = await Vacante.findById(id); 
+    const vacante = await Vacante.findById(_id); 
 
     if(verificarAutor(vacante, req.user)){
         //Si es el usuario y puede eliminar
